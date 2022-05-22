6c1ea069f8dcb92cefc0e2e044031ed94e10db58
G11-rifle <43462959+G11-rifle@users.noreply.github.com>
2022-05-22 17:39:16 +0900

    Add files via upload

---
+++ Simplex training program/kiso/stage2/CalcInt.java
@@ -0,0 +1,8 @@
+public class CalcInt {
+    public static void main(String[] args) {
+        int x = Integer.valueOf(args[0]);
+        int y = Integer.valueOf(args[1]);
+        int z = x + y;
+        System.out.println("答えは" + z);
+    }
+}
---
+++ Simplex training program/kiso/stage2/HeadMessage.java
@@ -0,0 +1,5 @@
+class HeadMessage {
+    public static void main(String[] args) {
+        System.out.println("\"" + args[0] + "\"からメッセージが届いています。今日も研修を頑張りましょう。");
+    }
+}
