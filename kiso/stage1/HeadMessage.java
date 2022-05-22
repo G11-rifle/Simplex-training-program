2231e53340c6bd7a51788eda15b03ea86391c69b
G11-rifle <43462959+G11-rifle@users.noreply.github.com>
2022-05-22 17:40:00 +0900

    Delete kiso directory

--- kiso/stage1/HeadMessage.java
+++
@@ -1,5 +0,0 @@
-class HeadMessage{
-    public static void main(String[] args){
-        System.out.println("\"シリウスさん\"からメッセージが届いています。今日も研修を頑張りましょう。");
-    }
-}
--- kiso/stage2/HeadMessage.java
+++
@@ -1,5 +0,0 @@
-class HeadMessage {
-    public static void main(String[] args) {
-        System.out.println("\"" + args[0] + "\"からメッセージが届いています。今日も研修を頑張りましょう。");
-    }
-}
